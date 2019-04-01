00013d8a3308c391314adc3cd8f8a2e0093eea49
Solenoid222 <solenoid2200@mail.ru>
2019-04-01 13:02:29 +0200

    -- First commit

---
+++ src/main/java/net/krasivoe/basicwebproject/Main.java
@@ -0,0 +1,7 @@
+package net.krasivoe.basicwebproject;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("Hello Basic Web Project!");
+    }
+}
---
+++ src/main/java/net/krasivoe/basicwebproject/web/UserServlet.java
@@ -0,0 +1,13 @@
+package net.krasivoe.basicwebproject.web;
+
+import java.io.IOException;
+
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
+    }
+}
